7d0eec3de9b1f5d77d706931dd4dea0b2f1783e6
Thiago Pinto <thiagopintodev@gmail.com>
2022-04-14 05:28:39 -0300

    styling pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,33 @@
 module ApplicationHelper
-  def current_role
-     user_signed_in? ? 'user' : 'guest'
+  def navbar_links
+    if user_signed_in?
+      navbar_links_for_user
+    else
+      navbar_links_for_guest
+    end
+  end
+
+  def navbar_links_for_guest
+    [
+      ["Home", guest_root_path, {}],
+      ["Log in", new_user_session_path, {}],
+      ["Sign up", new_user_registration_path, {}],
+      ["Forgot your password?", new_user_password_path, {}],
+    ]
+  end
+
+  def navbar_links_for_user
+    [
+      ["Home", user_root_path, {}],
+      ["Account", edit_user_registration_path, {}],
+      ["Sign out", destroy_user_session_path, {method: :DELETE, data: {confirm: 'Are you sure?'}}],
+    ]
+  end
+
+  def navbar_link_to(title, path, options)
+    is_active = current_page? path
+    options['class'] = "nav-link #{'active' if is_active}"
+    options['aria-current'] = "page" if is_active
+    link_to title, path, options
   end
 end
